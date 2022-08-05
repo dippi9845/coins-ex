14b8a2c9258c61966dc85c573e96f32b7221371c
Filippo Di Pietro <filippodp9845@gmail.com>
2022-08-05 17:22:15 +0200

    Create user.py

---
+++ src/ex/user.py
@@ -0,0 +1,24 @@
+
+class User:
+    def __init__(self) -> None:
+        pass
+
+    def register(self):
+        '''
+        perform a registration to an exchange
+        '''
+        pass
+
+    def sell(self):
+        '''
+        want to sell crypto
+        '''
+        pass
+
+    def buy(self):
+        '''
+        want to buy crypto
+        '''
+        pass
+
+    
