0831c4a16c2ade099e178947eb4a9df1c8bc90d2
Filippo Di Pietro <filippodp9845@gmail.com>
2022-08-05 23:29:06 +0200

    Update packet_trasmitter.py

--- src/ex/utils/packet_trasmitter.py
+++ src/ex/utils/packet_trasmitter.py
@@ -59,14 +59,14 @@ class PacketTransmitter:
     '''
     This class model a calss that is able to send and recive Packet
     '''
-    def __init__(self, buffer_size : int, bind : bool=False, addr : tuple[str, int]=None) -> None:
+    def __init__(self, buffer_size : int, bind : bool=False, bind_addr : tuple[str, int]=None) -> None:
         self.socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
         
         if bind:
-            if addr is None:
+            if bind_addr is None:
                 raise TypeError("Address can't be None if you need to bind the addres")
 
-            self.socket.bind(addr)
+            self.socket.bind(bind_addr)
         
         self.buffer_size = buffer_size
     
