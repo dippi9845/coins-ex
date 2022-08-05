f4f2312be8dc784c4c712ef8d2b287c27f7d2079
Filippo Di Pietro <filippodp9845@gmail.com>
2022-08-05 16:58:24 +0200

    Update exchange.py
    added a level of protection for all methods

--- src/ex/exchange.py
+++ src/ex/exchange.py
@@ -4,55 +4,55 @@ class Exchange:
     def __init__(self) -> None:
         pass
 
-    def register_user(self, name : str, surname : str, email : str, password : str, fiscal_code : str, nationality : str, telephone : str) -> bool:
-        '''
-        register an new user
-        '''
-        pass
-
-    def withdraw(self, atm_id : str):
-        '''
-        withdraw fiat money 
-        '''
-        pass
-
-    def send(self):
-        '''
-        send money to another user
-        '''
-        pass
-
-    def _make_transaction(self):
+    def __make_transaction(self):
         '''
         perform a generic transaction
         '''
         pass
-
-    def sell(self):
-        '''
-        sell a crypto
-        '''
-        pass
-
-    def buy(self):
-        '''
-        buy a crypto
-        '''
-        pass
-
-    def _create_account(self) -> str:
+    
+    def __create_account(self) -> str:
         '''
         create a generic account wallet / account fiat
         '''
         pass
 
-    def create_wallet(self) -> str:
+    def _register_user(self, name : str, surname : str, email : str, password : str, fiscal_code : str, nationality : str, telephone : str) -> bool:
+        '''
+        register an new user
+        '''
+        pass
+
+    def _withdraw(self, atm_id : str):
+        '''
+        withdraw fiat money 
+        '''
+        pass
+
+    def _send(self):
+        '''
+        send money to another user
+        '''
+        pass
+
+    def _sell(self):
+        '''
+        sell a crypto
+        '''
+        pass
+
+    def _buy(self):
+        '''
+        buy a crypto
+        '''
+        pass
+
+    def _create_wallet(self) -> str:
         '''
         create a wallet
         '''
         pass
 
-    def create_account_fiat(self) -> str:
+    def _create_account_fiat(self) -> str:
         '''
         create an account that contais only fiat money
         '''
