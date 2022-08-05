e777bf36d9c75f43234b525e727262bd48120658
Filippo Di Pietro <filippodp9845@gmail.com>
2022-08-05 18:43:17 +0200

    Update view.py
    added return type

--- src/ex/view.py
+++ src/ex/view.py
@@ -1,18 +1,19 @@
 from abc import abstractmethod
+from typing import Any
 
 class View:
 
     @abstractmethod
-    def ask_input(self, msg : str):
+    def ask_input(self, msg : str) -> str:
         pass
 
     @abstractmethod
-    def show_message(self, msg : str):
+    def show_message(self, msg : str) -> Any:
         pass
 
-    def menu(self, msg : str, choises : list[str], list_char = "-"):
+    def menu(self, msg : str, choises : list[str], list_char = "-") -> str:
         self.show_message(msg)
         self.show_message("\n")
         self.show_message(list_char)
         self.show_message(f"\n{list_char}".join(choises))
-        
+        return self.ask_input("")
