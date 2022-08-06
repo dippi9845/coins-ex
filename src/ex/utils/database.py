3c9d58cf045fb14d4fbea9b01c1a16ce5bd612f0
Filippo Di Pietro <filippodp9845@gmail.com>
2022-08-06 22:14:22 +0200

    Update database.py
    added a message to the user, once the initializetion is done

--- src/ex/utils/database.py
+++ src/ex/utils/database.py
@@ -65,3 +65,4 @@ if __name__ == "__main__":
     cursor.execute(f"CREATE DATABASE {DatabaseConfig['database_name']}")
     # crea l'utente
     cursor.execute(f"CREATE USER '{DatabaseConfig['username']}'@'{DatabaseConfig['host']}' IDENTIFIED BY '{DatabaseConfig['password']}'")
+    print("Done")
