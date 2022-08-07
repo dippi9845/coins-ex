0e8c21a7549cb8589ecde31c0f7f25e81db2bcaf
Filippo Di Pietro <filippodp9845@gmail.com>
2022-08-08 01:34:41 +0200

    Update init.sql
    all commands uppercase

--- init.sql
+++ init.sql
@@ -17,112 +17,112 @@ USE exchanges;
 -- _____________
 DROP TABLE IF EXISTS ATM;
 CREATE TABLE ATM (
-	Via varchar(255) not null,
-	Citta varchar(255) not null,
-	Provincia varchar(255) not null,
-	`Codice Icentificativo` varchar(255) not null,
-	Modello varchar(255) not null,
-	`Versione Software` varchar(255) not null,
-	`Spread attuale` int not null CHECK (`Spread attuale` > 0),
-	primary key (`Codice Icentificativo`));
+	Via VARCHAR(255) NOT NULL,
+	Citta VARCHAR(255) NOT NULL,
+	Provincia VARCHAR(255) NOT NULL,
+	`Codice Icentificativo` VARCHAR(255) NOT NULL,
+	Modello VARCHAR(255) NOT NULL,
+	`Versione Software` VARCHAR(255) NOT NULL,
+	`Spread attuale` INT NOT NULL CHECK (`Spread attuale` > 0),
+	PRIMARY KEY (`Codice Icentificativo`));
 
 DROP TABLE IF EXISTS ContoCorrente;
 CREATE TABLE ContoCorrente (
-	Indirizzo varchar(255) not null,
-	Saldo varchar(255) not null,
-	primary key (Indirizzo)
+	Indirizzo VARCHAR(255) NOT NULL,
+	Saldo VARCHAR(255) NOT NULL,
+	PRIMARY KEY (Indirizzo)
 );
 
 DROP TABLE IF EXISTS Wallet;
 CREATE TABLE Wallet (
-	Indirizzo varchar(255) not null,
-	Saldo varchar(255) not null,
-	primary key (Indirizzo)
+	Indirizzo VARCHAR(255) NOT NULL,
+	Saldo VARCHAR(255) NOT NULL,
+	PRIMARY KEY (Indirizzo)
 );
 
 DROP TABLE IF EXISTS Fiat;
 CREATE TABLE Fiat (
-	Nome varchar(255) not null,
-	Ticker varchar(255) not null,
-	primary key (Ticker));
+	Nome VARCHAR(255) NOT NULL,
+	Ticker VARCHAR(255) NOT NULL,
+	PRIMARY KEY (Ticker));
 
 DROP TABLE IF EXISTS Crypto;
 CREATE TABLE Crypto (
-	Nome varchar(255) not null,
-	Ticker varchar(255) not null,
-	primary key (Ticker));
+	Nome VARCHAR(255) NOT NULL,
+	Ticker VARCHAR(255) NOT NULL,
+	PRIMARY KEY (Ticker));
 
 DROP TABLE IF EXISTS Dipendente;
 CREATE TABLE Dipendente (
-	Carica varchar(255) not null,
-	Reparto varchar(255) not null,
-	Nome varchar(255) not null,
-	Cognome varchar(255) not null,
-	Residenza varchar(255) not null,
-	Matricola varchar(255) not null,
-	Salario int not null CHECK (Salario>0),
-	primary key (Matricola));
+	Carica VARCHAR(255) NOT NULL,
+	Reparto VARCHAR(255) NOT NULL,
+	Nome VARCHAR(255) NOT NULL,
+	Cognome VARCHAR(255) NOT NULL,
+	Residenza VARCHAR(255) NOT NULL,
+	Matricola VARCHAR(255) NOT NULL,
+	Salario INT NOT NULL CHECK (Salario>0),
+	PRIMARY KEY (Matricola));
 
 DROP TABLE IF EXISTS Exchange;
 CREATE TABLE Exchange (
-	Nome varchar(255) not null,
-	`Sede Operativa` varchar(255) not null,
-	`Sede Legale` varchar(255) not null,
-	Nazione varchar(255) not null,
-	`Sito web` varchar(255) not null,
-	Fondatore varchar(255) not null,
-	unique (`Sito web`),
-	unique (`Sede Operativa`),
-	unique (`Sede Legale`),
-	primary key (Nome));
+	Nome VARCHAR(255) NOT NULL,
+	`Sede Operativa` VARCHAR(255) NOT NULL,
+	`Sede Legale` VARCHAR(255) NOT NULL,
+	Nazione VARCHAR(255) NOT NULL,
+	`Sito web` VARCHAR(255) NOT NULL,
+	Fondatore VARCHAR(255) NOT NULL,
+	UNIQUE (`Sito web`),
+	UNIQUE (`Sede Operativa`),
+	UNIQUE (`Sede Legale`),
+	PRIMARY KEY (Nome));
 
 DROP TABLE IF EXISTS Ordine;
 CREATE TABLE Ordine (
-	`Tipo Ordine` ENUM("Vende", "Compra") not null,
-	Quantita int not null CHECK (Quantita > 0),
-	Data date not null,
-	Ora time not null,
-	primary key (Data, Ora));
+	`Tipo Ordine` ENUM("Vende", "Compra") NOT NULL,
+	Quantita INT NOT NULL CHECK (Quantita > 0),
+	Data date NOT NULL,
+	Ora time NOT NULL,
+	PRIMARY KEY (Data, Ora));
 
 DROP TABLE IF EXISTS Server;
 CREATE TABLE Server (
-	Host varchar(255) not null,
-	Porta int not null,
+	Host VARCHAR(255) NOT NULL,
+	Porta INT NOT NULL,
 	CONSTRAINT Hosting UNIQUE (Host,Porta));
 
 DROP TABLE IF EXISTS Transazione;
 CREATE TABLE Transazione (
-	Quantita int not null CHECK (Quantita > 0),
-	Ora date not null,
-	Data time not null,
-	primary key (Ora, Data),
-	unique (Ora, Data));
+	Quantita INT NOT NULL CHECK (Quantita > 0),
+	Ora date NOT NULL,
+	Data time NOT NULL,
+	PRIMARY KEY (Ora, Data),
+	UNIQUE (Ora, Data));
 
 DROP TABLE IF EXISTS Transazione_fisica;
 CREATE TABLE Transazione_fisica (
-	`Cambio attuale` int not null CHECK (`Cambio attuale` > 0),
-	Quantita int not null CHECK (Quantita>0),
-	Spread int not null CHECK (Spread > 0),
-	Data date not null,
-	Ora time not null,
-	primary key (Data, Ora));
+	`Cambio attuale` INT NOT NULL CHECK (`Cambio attuale` > 0),
+	Quantita INT NOT NULL CHECK (Quantita>0),
+	Spread INT NOT NULL CHECK (Spread > 0),
+	Data date NOT NULL,
+	Ora time NOT NULL,
+	PRIMARY KEY (Data, Ora));
 
 DROP TABLE IF EXISTS Utente;
 CREATE TABLE Utente (
-	ID int AUTO_INCREMENT,
-	Email varchar(255) not null,
-	Password varchar(255) not null,
-	Nome varchar(255) not null,
-	Cognome varchar(255) not null,
-	Nazionalita varchar(255) not null,
-	Residenza varchar(255) not null,
-	`Numero Di Telefono` varchar(255) not null,
-	`Data di nascita` date not null,
-	`Codice Fiscale` varchar(255) not null,
-	primary key (ID),
-	unique (`Codice Fiscale`));
+	ID INT AUTO_INCREMENT,
+	Email VARCHAR(255) NOT NULL,
+	Password VARCHAR(255) NOT NULL,
+	Nome VARCHAR(255) NOT NULL,
+	Cognome VARCHAR(255) NOT NULL,
+	Nazionalita VARCHAR(255) NOT NULL,
+	Residenza VARCHAR(255) NOT NULL,
+	`Numero Di Telefono` VARCHAR(255) NOT NULL,
+	`Data di nascita` date NOT NULL,
+	`Codice Fiscale` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (ID),
+	UNIQUE (`Codice Fiscale`));
 
--- Constraints Section
+-- ConstraINTs Section
 -- ___________________
 
 
