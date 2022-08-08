9957dc156fb0953d672e92fa35e37c1e02393832
Filippo Di Pietro <filippodp9845@gmail.com>
2022-08-08 17:46:40 +0200

    added abstract method menu in view

--- src/ex/view.py
+++ src/ex/view.py
@@ -11,6 +11,19 @@ class View:
     def show_message(self, msg : str) -> Any:
         pass
 
+    @abstractmethod
+    def menu(self, msg : str, choises : list[str]) -> str:
+        pass
+
+
+class TerminalView(View):
+
+    def ask_input(self, msg: str) -> str:
+        return input(msg)
+
+    def show_message(self, msg: str) -> Any:
+        return print(msg)
+    
     def menu(self, msg : str, choises : list[str], list_char = "-") -> str:
         self.show_message(msg)
         #self.show_message(list_char)
@@ -23,12 +36,3 @@ class View:
         else:
             self.show_message(f"{ch} is not an option, retry")
             return self.menu(msg, choises, list_char=list_char)
-
-
-class TerminalView(View):
-
-    def ask_input(self, msg: str) -> str:
-        return input(msg)
-
-    def show_message(self, msg: str) -> Any:
-        return print(msg)
